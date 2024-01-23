4b90d17c63674c350f7e0c683c5d9f2da24861cb
kubs0135 <kubskathayat01@gmail.com>
2024-01-23 18:58:23 +0545

    learned abourt variables, comment and console

---
+++ index.js
@@ -0,0 +1,34 @@
+console.log("hello world"); //prints the msg
+//variables
+//var(es5)
+var kuberDada ="kubs"; //always use camelCase for joining two or more words for variables
+kuberDada="kk"; //second or more xoti data type dinupardaina
+console.log(kuberDada);
+
+//let,const(es6)
+let teacher="sir";
+teacher="reactsir";
+console.log(teacher);
+const teach="sir";
+console.log(teach);
+
+
+//create 4 variables using const keyword for table ,fan, chair and pen
+/* 
+multi-line comment
+
+const kuberClass="dhvch";
+const rollStudent="11";
+
+*/
+const tableBlack="ab";
+const fanKk="tyo";
+const chair="4 khutte";
+const pen="cello";
+console.log(tableBlack);
+console.log(fanKk);
+console.log(chair);
+console.log(pen);
+console.log(fanKk,chair,pen,tableBlack);
+
+//using var and let hw for 4 variables
